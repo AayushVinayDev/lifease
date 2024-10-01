8dc33aafabb7a4d3bb50e6d2be009b7de40248b9
Aayush Vinay <btech10104.19@bitmesra.ac.in>
2024-10-01 09:34:09 +0530

    correction

--- backend/src/players/entities/player.entity.ts
+++ backend/src/players/entities/player.entity.ts
@@ -11,6 +11,21 @@ export class Player {
   @Column()
   team: string;
 
+  @Column({ default: 0 })
+  runsScored: number;
+
+  @Column({ default: 0 })
+  ballsFaced: number;
+
+  @Column({ default: 0 })
+  runsConceded: number;
+
+  @Column({ default: 0 })
+  wicketsTaken: number;
+
+  @Column({ default: 0 })
+  ballsBowled: number;
+
   @Column()
   role: 'batsman' | 'bowler' | 'all-rounder';
 }
--- backend/src/scores/scores.module.ts
+++ backend/src/scores/scores.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ScoresController } from './scores.controller';
 import { ScoresService } from './scores.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Score } from './entities/score.entity';
+import { Innings } from '../matches/entities/innings.entity';
+import { Player } from '../players/entities/player.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Score, Innings, Player])],
   controllers: [ScoresController],
   providers: [ScoresService]
 })
+
 export class ScoresModule {}
